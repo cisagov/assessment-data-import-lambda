ef56d6243934a477b0c7ca03e83af9b9c1cc5d3d
Dave Redmin <david.redmin@beta.dhs.gov>
2019-12-05 10:31:29 -0500

    Add additional test ("test_release_version") from skeleton

--- tests/test_adi.py
+++ tests/test_adi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env pytest -vs
 """Tests for example."""
 
+import os
 import sys
 from unittest.mock import patch
 
@@ -9,6 +10,7 @@ import pytest
 from adi import assessment_data_import as adi
 
 # define sources of version string
+RELEASE_TAG = os.getenv("RELEASE_TAG")
 PROJECT_VERSION = adi.__version__
 
 
@@ -21,3 +23,13 @@ def test_stdout_version(capsys):
     assert (
         captured.out == f"{PROJECT_VERSION}\n"
     ), "standard output by '--version' should agree with module.__version__"
+
+
+@pytest.mark.skipif(
+    RELEASE_TAG in [None, ""], reason="this is not a release (RELEASE_TAG not set)"
+)
+def test_release_version():
+    """Verify that release tag version agrees with the module version."""
+    assert (
+        RELEASE_TAG == f"v{PROJECT_VERSION}"
+    ), "RELEASE_TAG does not match the project version"
