007a855289bfdec9c0264397e7e0829c0bb9f577
Michael Glenn <mbg198618@gmail.com>
2021-06-06 06:22:37 -0500

    Update build.sh-.js

--- docker-compose/build.sh-.js
+++ docker-compose/build.sh-.js
@@ -23,3 +23,47 @@ open-sourceprojects.
 to help strengthen the security and resiliency of our Nation. 
 'With' your help, 
   we commit today and secure tomorrow.
+get:
+runtime-ci
+ref	af39fd5f1a786c570385a926c293295edcdc9a69
+get:
+cf-k8s-pr-all-branches-and-forks
+approved_review_count	0
+commit	0845c180a3f5c1961138506d9a24e9be2eb80d66
+committed	2021-06-06T08:32:32Z
+pr	669
+state	OPEN
+get:
+cf-k8s-ci
+ref	8dfa9077d7f78388cd3e5db9ff8273ebe2e0f718
+put:
+cf-k8s-pr-all-branches-and-forks
+approved_review_count	0
+commit	0845c180a3f5c1961138506d9a24e9be2eb80d66
+committed	2021-06-06T08:32:32Z
+pr	669
+state	OPEN
+get:
+cf-k8s-pr-all-branches-and-forks
+approved_review_count	0
+commit	0845c180a3f5c1961138506d9a24e9be2eb80d66
+committed	2021-06-06T08:32:32Z
+pr	669
+state	OPEN
+task:
+check-vendir-sync
+put:
+cf-k8s-pr-all-branches-and-forks
+get:
+cf-k8s-pr-all-branches-and-forks
+task:
+write-pr-check-failure-comment
+put:
+cf-k8s-pr-all-branches-and-forks
+approved_review_count	0
+commit	0845c180a3f5c1961138506d9a24e9be2eb80d66
+committed	2021-06-06T08:32:32Z
+pr	669
+state	OPEN
+get:
+cf-k8s-pr-all-branches-and-forks
